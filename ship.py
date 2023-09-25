d406dc6035b4b560a6321ec9f1debf0b06684395
John Tolczyk <johntolczyk@gmail.com>
2023-09-25 01:51:09 -0500

    added ship to screen

--- ship.py
+++ ship.py
@@ -1,18 +1,19 @@
 import pygame
 
+
 class Ship:
-    """ A class to manage the ship."""
+    """A class to manage the ship."""
 
     def __init__(self, ai_game):
-        """Initialize the ship and its starting position."""
+        """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
 
-        #load the ship image and get it's rect.
+        # Load the ship image and get its rect.
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
 
-        # Start each new ship at the bottom of the screen.
+        # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
 
     def blitme(self):
